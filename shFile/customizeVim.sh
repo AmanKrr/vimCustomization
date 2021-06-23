7f25558b0fb937b7c5b7fd7716461bb2c8fd2f14
Amankr <54282666+AMAN74458@users.noreply.github.com>
2021-06-23 23:23:17 +0530

    Update customizeVim.sh

--- shFile/customizeVim.sh
+++ shFile/customizeVim.sh
@@ -19,8 +19,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/vim/bundle/Vundle.vim
 vim -c 'PluginInstall' -c 'q' -c 'q'
 
 sudo apt install build-essential cmake vim-nox python3-dev
-sudo apt install mono-complete golang nodejs default-jdk npm
-sudo apt-get install python
+sudo apt install mono-complete default-jdk
 cd ~/vim/bundle/YouCompleteMe
 python3 install.py --all
 echo "Final Touch"
