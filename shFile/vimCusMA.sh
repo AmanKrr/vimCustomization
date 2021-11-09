c2cb2e8d82f297fe38cab760dedc2b46c61db4a9
Amankr <54282666+AmanKrr@users.noreply.github.com>
2021-11-10 00:05:24 +0530

    For Mac and Linuz

---
+++ shFile/vimCusMA.sh
@@ -0,0 +1,87 @@
+# .sh file for customizing vim [zsh mac] 
+# Run this file while you are in root [root symbol in unix ~ or your username].
+
+# Checking OS TYPE. 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Installing vim in linux.
+    echo "Installing VIM"
+    echo
+    sudo apt-get install vim
+    echo
+    echo "Installing CMake, vim-nox, python3, mono-complete, go-lang, nodejs, default-jdk, npm"
+    echo
+    sudo apt install build-essential cmake vim-nox python3-dev
+    sudo apt install mono-complete default-jdk
+    echo
+else [[ "$OSTYPE" == "darwin"* ]]; then
+    # Checking Homebrew is installed or not. If installed then update else install.
+    if [[ $(command -v brew) == "" ]]; then
+        echo "Installing Homebrew"
+        echo
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo
+        # Installing Vim. Pre-installed macOS system Vim does not support Python 3. So you need to install either a Vim that supports Python 3.
+        echo "Installing VIM"
+        echo
+        brew install vim cmake 
+        echo
+    else
+        echo "Updating Homebrew"
+        echo
+        brew update
+        echo
+        # Installing Vim. Pre-installed macOS system Vim does not support Python 3. So you need to install either a Vim that supports Python 3.
+        echo "Installing VIM"
+        echo
+        brew install vim cmake   
+        echo
+    fi
+fi
+
+# Cloning vimCustomization folder. Then moving vimrc file in root and renaming to hide it.
+git clone https://github.com/AmanKrr/vimCustomization
+cd vimCustomization
+mv vimrc ~/
+cd ~ 
+mv vimrc .vimrc
+echo
+
+# Checking directory exits or not.
+FILE=~/.vim
+if [ -d "$FILE" ]; then
+    echo "Vim folder exists."
+    echo
+else
+    echo "Vim folder not exits. Creating vim folder in $HOME/"
+    mkdir ~/.vim
+    echo "Done"
+    echo
+fi
+
+# Set up vbundle, then executing command in vim to intall plugins.
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c 'PluginInstall' -c 'q' -c 'q'
+
+# Compile YCM
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all 
+
+# Removing .vimrc and updating it.
+echo "Updating .vimrc"
+rm -rf .vimrc
+cd vimCustomization/updated
+mv vimrc ~/
+cd ~
+mv vimrc .vimrc
+echo
+
+# Moving color folder into vim folder
+cd ~
+mv colors ~/.vim/
+cd ~
+
+# Cleaning
+echo "Cleaning. After finishing vim will launch."
+rm -rf vimCustomization
+echo
+
