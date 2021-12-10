a7e4d3d80bc3199e5090db58dfbf2a84aef66668
Amankr <54282666+AmanKrr@users.noreply.github.com>
2021-12-10 21:05:11 +0530

    updated

--- clean.sh
+++ clean.sh
@@ -7,78 +7,81 @@ redE="\e[0m"
 # Checking OS TYPE. 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Installing vim in linux.
-    echo
-    echo -e "${redS}Removing VIM ${redE}"
-    echo
+    printf "\n"
+    printf "${redS}Removing VIM ${redE}"
+    printf "\n"
     sudo apt-get remove vim
-    echo
-    echo -e "${redS}Removing CMake, vim-nox, python3, mono-complete, go-lang, nodejs, default-jdk, npm ${redE}"
-    echo
+    printf "\n"
+    printf "${redS}Removing CMake, vim-nox, python3, mono-complete, go-lang, nodejs, default-jdk, npm ${redE}"
+    printf "\n"
     sudo apt-get remove build-essential
     sudo apt autoremove
-    echo
+    printf "\n"
     sudo apt-get remove cmake
     sudo apt autoremove
-    echo
+    printf "\n"
     sudo apt-get remove vim-nox
     sudo apt autoremove
-    echo
+    printf "\n"
     sudo apt-get remove python3-dev
     sudo apt autoremove
-    echo
+    printf "\n"
     sudo apt-get remove mono-complete 
     sudo apt autoremove 
-    echo
+    printf "\n"
     sudo apt-get remove default-jdk
     sudo apt autoremove
-    echo
+    printf "\n"
 else [[ "$OSTYPE" == "darwin"* ]]
     # Removing packages installed using Homebrew
-    echo -e "${redS} Removing Vim. ${redE}"
+    printf "${redS} Removing Vim. ${redE}"
     brew remove vim
-    echo
-    echo -e "${redS} Removing CMake. ${redE}"
+    printf "\n"
+    printf "${redS} Removing CMake. ${redE}"
     brew remove cmake
-    echo
-    echo -e "${redS} Removing dependencies. ${redE}"
+    printf "\n"
+    printf "${redS} Removing dependencies. ${redE}"
     brew autoremove
-    echo
+    printf "\n"
 fi
 
 # Checking directory exits or not.
 FILE=~/.vim
 if [ -d "$FILE" ]; then
-    echo -e "${redS}Vim folder already exists, Removing...... ${redE}"
+    printf "${redS}Vim folder already exists, Removing...... ${redE}"
     rm -rf ~/.vim
-    echo
+    printf "\n"
 else
-    echo -e "${redS}Vim folder already removed. ${redE}"
-    echo
+    printf "${redS}Vim folder already removed. ${redE}"
+    printf "\n"
 fi
 
 # Removing .vimrc and updating it.
-echo -e "${redS}Removing .vimrc .viminfo ${redE}"
+printf "${redS}Removing .vimrc .viminfo ${redE}"
 cd ~
 rm -rf .vimrc
 rm -rf .viminfo
-echo
+printf "\n"
 
 FILES=~/tmp
 if [ -d "$FILES" ]; then
-    echo -e "${redS}tmp folder already exists, Removing....... [created for swap files]${redE}"
+    printf "${redS}tmp folder already exists, Removing....... [created for swap files]${redE}"
     rm -rf ~/tmp
-    echo
+    printf "\n"
 else
-    echo -e "${redS}tmp folder already removed. $HOME/ ${redE}"
-    echo "Done"
-    echo
+    printf "${redS}tmp folder already removed. $HOME/ ${redE}"
+    printf "Done"
+    printf "\n"
 fi
 
 vim --version
-echo "SUCCESS"
-echo
-echo -e "${redS}For macOS, Homebrew is not removed. If you wish to remove it run this in your terminal => ${redE}"
-echo
-echo '  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"'
-rm -rf ~/clean.sh
-echo
+
+if [[ $(command -v vim) == "" ]]; then
+    printf "SUCCESS"
+else
+    printf "Failed Cleaning"
+fi
+
+printf "SUCCESS"
+printf '${redS} For macOS, Homebrew is not removed. If you wish to remove it run this in your terminal =>. ${redE} /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)" '
+printf "\n"
